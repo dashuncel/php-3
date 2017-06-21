d31b8f5e5eaaa4333a0ec5d922209e836f01931e
dashuncel <korovkinada@ya.ru>
2017-06-21 15:11:59 +0800

    Исправления. Часть 4

--- dz3_arrays.php
+++ dz3_arrays.php
@@ -3,46 +3,31 @@
 include 'dz3_ini.php';
 
 $double_animals=[]; // массив с названиями зверей из двух слов
-$mystic_animals=[]; // массив фантастических зверей
-$list_used=[];      // список использованых индексов
-$list_places=[];    // собранный многомерный массив "место" - "список животных"
+$first_parts=[];    // первые части
+$second_parts=[];   // вторые части
 
 // 1. массив из зверей с названиями из двух слов:
 print_r("</br></br><h1>Массив названий, состоящх более из двух слов:</h1></br><pre>");
 foreach($animals as $key => $val) {
   $double_animals[$key]=[];
-  $list_used[$key]=[];
   foreach($val as $sub_key => $sub_val) {
-    $new_arr=explode($delimiter,trim($sub_val)); 
+    $new_arr=explode($delimiter,$sub_val); 
     if (count($new_arr) == 2) {
       $double_animals[$key][]=$sub_val;
+      $second_parts[]=$new_arr[1];
+      $first_parts[]=$new_arr[0];
     }
   }
 }
 print_r($double_animals);
 
-// перемешиваем слова в Элементах:
-print_r("</pre></br></br><h1>3. Массив фантастических зверей:</h1></br><pre>");
-foreach($double_animals as $key => $val) {
-  $mystic_animals[$key]=[];
-  foreach($val as $sub_val) {
-    $second_part='';
-    do {
-      $ran_x=array_rand($double_animals); // первый сучайный именованный ключ
-      $ran_y=array_rand($double_animals[$ran_x]); // второй порядковый ключ
-      $result=array_search($ran_y, $list_used[$ran_x]); // результат поиска индекса в массиве использованных
-
-      if ($result == false and is_bool($result)) { // если индекс не найден, или элемент не использован
-        $list_used[$ran_x][]=$ran_y; // записываем в массив найденный индекс
-        $second_part=explode($delimiter,trim($double_animals[$ran_x][$ran_y]))[1]; // запоминаем случайно выбранный уникальный элемент
-      };
-    } while ($second_part=='');
-
-    $mystic_animals[$key][]=explode($delimiter,trim($sub_val))[0]." ".$second_part; // первое слово текущего элемента
-  }
+shuffle($second_parts);
+shuffle($first_parts);
+print_r("</pre></br></br><h1>3. Фантастические звери:</h1></br><pre>");
+foreach ($first_parts as $key => $val) {
+  $first_parts[$key]=$val." ".$second_parts[$key];
 }
-
-print_r($mystic_animals);
+print_r($first_parts);
 
 // дополнительно:
 print_r("</pre></br></br><h1>Дополнительно. Места обитания</h1></br>");
